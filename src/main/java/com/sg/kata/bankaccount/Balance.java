ef08d7eff2edb59f6dbcd660683400925d383fab
Salwa Chaabi <salwa.chaabi@gmail.com>
2022-03-10 11:19:31 +0100

    add equals and hashcode methods to fix deposit operation test

--- src/main/java/com/sg/kata/bankaccount/Balance.java
+++ src/main/java/com/sg/kata/bankaccount/Balance.java
@@ -15,4 +15,28 @@ public class Balance {
 		}
 		throw new IllegalArgumentException("Operation is not supported");
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (balanceAfterOperation ^ (balanceAfterOperation >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Balance other = (Balance) obj;
+		if (balanceAfterOperation != other.balanceAfterOperation)
+			return false;
+		return true;
+	}
+	
+	
 }
